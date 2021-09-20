d2fdcd42381940b9ac505547d16f495cae438fcf
tmassart873 <tim.massart@hotmail.com>
2021-09-20 21:18:58 +0200

    Create binary_conveerter.java

---
+++ First Programs/binary_conveerter.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Converter{
+
+    static String toBinary(int num) {
+
+        String binary="";
+
+        while(num > 0) {
+
+        binary = (num%2)+binary;
+        num /= 2;
+        }
+
+        return binary;
+    }
+}
+
+
+public class Program {
+    public static void main(String[ ] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.print(Converter.toBinary(x));
+    }
+}
